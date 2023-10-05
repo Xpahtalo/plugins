b0c66fb98ffdf32ed28ea860ab8b3095993869f8
Xpahtalo <106037495+Xpahtalo@users.noreply.github.com>
2023-10-04 23:14:13 -0700

    Update generate_pluginmaster.py

--- generate_pluginmaster.py
+++ generate_pluginmaster.py
@@ -6,7 +6,7 @@ from os.path import getmtime
 from zipfile import ZipFile, ZIP_DEFLATED
 
 BRANCH = os.environ['GITHUB_REF'].split('refs/heads/')[-1]
-DOWNLOAD_URL = 'https://github.com/daemitus/MyDalamudPlugins/raw/{branch}/plugins/{plugin_name}/latest.zip'
+DOWNLOAD_URL = 'https://github.com/Xpahtalo/plugins/raw/{branch}/plugins/{plugin_name}/latest.zip'
 
 DEFAULTS = {
     'IsHide': False,
